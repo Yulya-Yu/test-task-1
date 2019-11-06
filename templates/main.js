88e4229e4af70c12d3a871deb1dac75874e20332
Yuliya <kuroi.kasa1509@gmail.com>
2019-11-06 17:48:02 +0700

    fix styling issues

--- templates/main.js
+++ templates/main.js
@@ -1,11 +1,12 @@
-
 const burger = document.querySelector(".burger");
 const navLinks = document.querySelector(".nav-links");
 const links = document.querySelectorAll(".nav-links li");
 
+
 burger.addEventListener("click", () => {
-  navLinks.classList.toggle("open");
-  links.forEach(link => {
-    link.classList.toggle("fade");
-  });
+    navLinks.classList.toggle("open");
+    burger.classList.toggle("open");
+    links.forEach(link => {
+        link.classList.toggle("fade");
+    });
 });
